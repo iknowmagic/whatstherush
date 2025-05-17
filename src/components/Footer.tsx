864fa1ba271ddaffe017247f76b5d70dfef75f9d
iknowmagic <ilan@iknowmagic.net>
2025-05-16 18:26:09 -0700

    feat: remove footer links and copyright information for a cleaner design

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,8 +2,6 @@ import React from "react";
 import Link from "next/link";
 
 const Footer: React.FC = () => {
-  const currentYear = new Date().getFullYear();
-
   return (
     <footer className="bg-neutral py-12 font-body text-neutral-content/80">
       <div className="mx-auto px-4 md:px-8 container">
@@ -42,35 +40,6 @@ const Footer: React.FC = () => {
             >
               Contact
             </Link>
-          </div>
-
-          <div className="flex md:flex-row flex-col justify-between items-center pt-8 border-neutral-content/20 border-t text-neutral-content/50 text-xs">
-            <div>
-              &copy; {currentYear} What&apos;s The Rush. All rights reserved.
-            </div>
-
-            <div className="flex space-x-6 mt-4 md:mt-0">
-              <Link
-                href="/privacy"
-                className="hover:text-neutral-content transition-colors"
-              >
-                Privacy
-              </Link>
-              <Link
-                href="/terms"
-                className="hover:text-neutral-content transition-colors"
-              >
-                Terms
-              </Link>
-              <a
-                href="https://github.com/whatstherush"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="hover:text-neutral-content transition-colors"
-              >
-                GitHub
-              </a>
-            </div>
           </div>
         </div>
       </div>
