30734b2e83d5afab9747bb80b6fbe77ae4165532
iknowmagic <ilan@iknowmagic.net>
2025-05-26 14:46:47 -0700

    refactor: simplify SectionBlock layout and remove unnecessary conditional rendering

--- src/components/SectionBlock.tsx
+++ src/components/SectionBlock.tsx
@@ -19,41 +19,29 @@ const SectionBlock: React.FC<SectionBlockProps> = ({
 
   return (
     <div
-      className={`flex w-full ${
+      className={`grid grid-cols-2 place-items-center h-[170px] w-full ${
         isDark ? "bg-dark text-light" : "bg-light text-dark"
       }`}
     >
-      {flip && (
-        <div className="flex justify-start items-center pl-1 h-full">
-          <Image
-            src={image}
-            width={170}
-            height={170}
-            alt={alt}
-            className="object-cover"
-          />
-        </div>
-      )}
-
+      {/* TEXT */}
       <div
-        className={`flex flex-1 items-center${!flip ? " pl-4 md:pl-12 lg:pl-24 tablet:pl-6 desktop:pl-8" : " pr-4 md:pr-12 lg:pr-24 tablet:pr-6 desktop:pr-8"}`}
+        className={`font-inconsolata leading-tight ${
+          flip
+            ? "order-2 justify-self-start pr-4"
+            : "order-1 justify-self-end pl-4"
+        }`}
       >
-        <div className="max-w-[200px] desktop:max-w-[400px] tablet:max-w-[300px] font-inconsolata text-body desktop:text-body tablet:text-body leading-[1.2em]">
-          {children}
-        </div>
+        {children}
       </div>
 
-      {!flip && (
-        <div className="flex justify-end items-center pr-1 desktop:pr-6 tablet:pr-4 w-image desktop:w-image tablet:w-image h-full">
-          <Image
-            src={image}
-            width={170}
-            height={170}
-            alt={alt}
-            className="w-image desktop:w-image tablet:w-image h-image desktop:h-image tablet:h-image object-cover"
-          />
-        </div>
-      )}
+      {/* IMAGE */}
+      <Image
+        src={image}
+        width={170}
+        height={170}
+        alt={alt}
+        className={`object-cover ${flip ? "order-1" : "order-2"}`}
+      />
     </div>
   );
 };
